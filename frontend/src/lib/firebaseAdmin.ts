c6d128517bf9c7315b34b8e19e2432dc4f571040
D3v hustle <trade@nexxgennhustle.com>
2025-09-24 08:23:04 +0000

    Add reCAPTCHA verification + unlock API + Firebase admin integration

---
+++ frontend/src/lib/firebaseAdmin.ts
@@ -0,0 +1,11 @@
+import * as admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
+    ),
+  });
+}
+
+export const db = admin.firestore();
